5ee162c9282e29a628ff423fbaeb9c577ed92903
muhtar-sgk <muhtarrn@gmail.com>
2023-09-04 14:20:26 +0700

    Section 1 : Basic of state management using Riverpod

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+final appCount = StateProvider<int>((ref) {
+  return 1;
+});
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int count = ref.watch(appCount);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Riverpod App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$count',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            heroTag: 'One',
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => const SecondPage()
+                )
+              );
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.arrow_right_rounded),
+          ),
+          FloatingActionButton(
+            heroTag: 'two',
+            onPressed: () {
+              ref.read(appCount.notifier).state++;
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends ConsumerWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int count = ref.watch(appCount);
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Center(
+          child: Text('$count', style: TextStyle(
+            fontSize: 32
+          ),)
+        )
+      ),
+    );
+  }
+}
